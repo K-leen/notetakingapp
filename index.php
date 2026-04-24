4e39b13577c366f8250508712f4d2cc877b5862b
K-leen <rollekanda2@gmail.com>
2026-04-24 14:04:30 +0300

    FRIDAY

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NOTETAKINGAPP❤️</title>
+    <link rel="stylesheet" href="./Styles/bootstrap.min.css">
+</head>
+<body class="bg-dark text-white flex justify-content-center align-items-center p-5 container">
+    <div class="card">
+        <p class="card-title">NOTE TAKING APP😁👌</p>
+        <label for="note">Enter note😘</label>
+        <textarea class="form-group-3"name="note" id="note" rows="10" placeholder="What hsppened today..."></textarea>
+        <button type="submit" class="btn btn-success mt-3">SAVE</button>
+    <p class="display-1">BOOTSTRAP IS AMAZING 😊😘</p>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/jquery.js"></script>
+    <script src="./js/mainquery.js"></script>
+</body>
+</html>
